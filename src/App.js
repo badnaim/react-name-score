a2f14cd4ac1ae8988da86c4f0dbc57cc866f312b
badnaim <batnyaam.tumurbat@gmail.com>
2023-01-10 12:10:05 +0800

    b

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [score, setScore] = useState(0);
+  const [users, setUsers] = useState(UsersList);
+
+  function decrement() {
+    setScore(score - 1);
+  }
+  function increment() {
+    setScore(func1 + 1);
+  }
+  function add() {
+    setNumber(number + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <div className="header">
+          <div>Total players: {number}</div>
+          <div>
+            <button>By name</button>
+          </div>
+          <div>
+            <button>By score</button>
+          </div>
+        </div>
+        {users.map((user, index) => (
+          <div className="added" key={index}>
+            <div id="one">#{index + 1}</div>
+            <div id="two">{user.name}</div>
+            <div className="score">
+              <button id="btn" onClick={decrement}>
+                -
+              </button>
+              {user.score}
+              <button id="btn" onClick={increment}>
+                +
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button onClick={add}>Add player</button>
     </div>
   );
 }
---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function User() {
+  const UsersList = [
+    { name: "gonchig", score: 10 },
+    { name: "gonchig1", score: 15 },
+    { name: "gonchig2", score: 20 },
+    { name: "gonchig3", score: 25 },
+  ];
+
+  return <div></div>;
+}
